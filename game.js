60ccbd0bdf44af48b2868096e28c219df46af6e7
Ben Rowe <brow495@gmail.com>
2020-06-17 11:34:52 -0600

    Add answer functionality

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 baseURL = "http://localhost:3000";
 superherosURL = `${baseURL}/superheros`;
+questionsURL = `${baseURL}/questions`;
 
 const superheroSection = document.querySelector("section");
 
@@ -8,6 +9,43 @@ fetch(superherosURL)
   .then(parseJSON)
   .then(displaySuperheros);
   
+fetch(questionsURL)
+  .then(parseJSON)
+  .then(displayQuestion)
+
+const questionSection = document.querySelector("h1")
+  
+function displayQuestion(question){
+  questionSection.appendChild(question.text)
+  // answerQuestion(question)
+  const questionNumber = question.number
+
+  return questionNumber
+}
+
+function answerQuestion(players){
+  switch (questionNumber) {
+    case 1: 
+      const winner = players.map(&:speed).max;
+      break;
+    case 2: 
+      const winner = players.map(&:intelligence).max;
+      break;
+    case 3:
+      const winner = players.map {|player| player.intelligence + player.durability};
+      break;
+    case 4:
+      const winner = players.map(&:speed).min;
+      break;
+    case 5:
+      const winner = players.map(&:strength).max
+      break;
+    case 6:
+      const winner = players.map(&:intelligence).min
+      break;
+  }
+  return winner
+}
 
 function displaySuperheros(superheros) {
   superheros.forEach(showSuperhero);
@@ -33,6 +71,12 @@ function showSuperhero(superhero) {
 
   const occupationlabel = document.createElement('h5');
   occupationlabel.textContent = "Occupation:";
+
+  if (answer === true){
+    superheroCard.innerHTML = `<a href="http://localhost:3000/response.html?answer=true"/>`
+  } else {
+    superheroCard.innerHTML = `<a href="http://localhost:3000/response.html?answer=false"/>`
+  }
 
 
 
--- right.js
+++
@@ -1,2 +0,0 @@
-const response = document.querySelector('#response')
-

0f286a606940ce19223b305598711fadf28f0c4b
Ben Rowe <brow495@gmail.com>
2020-06-18 09:26:55 -0600

    Add score display at end of quiz

--- response.js
+++ response.js
@@ -3,20 +3,27 @@ const searchParams = new URLSearchParams(window.location.search)
 
 const answer = document.querySelector('#response')
 
-
+let count = 0;
+let score = 0;
 
 if (result === "true"){
     const rightAnswer = document.createElement('h6');
     rightAnswer.textContent = "RIGHT!";
-    document.body.prepend(rightAnswer)
+    document.body.prepend(rightAnswer);
+    score += 1;
 } else {
     const wrongAnswer = document.createElement('h6');
     wrongAnswer.textContent = "WRONG!";
-    document.body.prepend(wrongAnswer)
+    document.body.prepend(wrongAnswer);
+}
+
+const scoreDisplay = document.querySelector("#score-display")
     
-function increaseCount(count){
-    count +=1
+function increaseCount(increase){
+    count += increase
     if (count === 5) {
-        
+        const display = document.createElement('p')
+        display.textContent = `${score}/${count}`
+        scoreDisplay.appendChild(display)
     }
 }
